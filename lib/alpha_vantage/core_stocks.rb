6a50270895052b3c00a80944d1c31b68f357779c
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-03-22 18:37:29 -0300

    chore: Refactor AlphaVantage CoreStocks class and Base module

--- lib/alpha_vantage/core_stocks.rb
+++ lib/alpha_vantage/core_stocks.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 # CoreStocks APIs integration
-  module AlphaVantage 
-    class CoreStocks < Base
-      def symbol_search(keywords:)
-        get(params: { function: 'SYMBOL_SEARCH', keywords: keywords })
-      end 
+module AlphaVantage
+  # CoreStocks class
+  class CoreStocks < AlphaVantage::Base
+    def symbol_search(keywords:)
+      get(params: { function: 'SYMBOL_SEARCH', keywords: })['data']
+    end
 
-      def time_series_daily_adjusted(symbol:)
-        get(params: { function: 'TIME_SERIES_DAILY_ADJUSTED', symbol: symbol })
-      end
+    def time_series_daily_adjusted(symbol:)
+      get(params: { function: 'TIME_SERIES_DAILY_ADJUSTED', symbol: })['data']
+    end
 
-      def global_quote(symbol:)
-        get(params: { function: 'GLOBAL_QUOTE', symbol: symbol })
-      end
+    def global_quote(symbol:)
+      get(params: { function: 'GLOBAL_QUOTE', symbol: })['data']
     end
   end
+end
---
+++ spec/lib/alpha_vantage/core_stocks_spec.rb
@@ -0,0 +1,232 @@
+require 'rails_helper'
+
+RSpec.describe AlphaVantage::CoreStocks do
+  describe '#symbol_search' do
+    context 'when the request is successful' do
+      context 'when request returns a single match' do
+        it 'returns the search results' do
+          VCR.use_cassette('alpha_vantage/symbol_search_success_unique') do
+            response = described_class.new.symbol_search(keywords: 'petr4')
+
+            expect(response).to eq(
+              { 'bestMatches' => [
+                {
+                  '1. symbol' => 'PETR4.SAO',
+                  '2. name' => 'Petróleo Brasileiro S.A. - Petrobras',
+                  '3. type' => 'Equity',
+                  '4. region' => 'Brazil/Sao Paolo',
+                  '5. marketOpen' => '10:00',
+                  '6. marketClose' => '17:30',
+                  '7. timezone' => 'UTC-03',
+                  '8. currency' => 'BRL',
+                  '9. matchScore' => '0.7692'
+                }
+              ] }
+            )
+          end
+        end
+      end
+
+      context 'when request returns multiple matches' do
+        it 'returns the search results' do
+          VCR.use_cassette('alpha_vantage/symbol_search_success_multiple') do
+            response = described_class.new.symbol_search(keywords: 'bitcoin')
+
+            expect(response).to eq(
+              {
+                'bestMatches' => [
+                  {
+                    '1. symbol' => 'EBIT-U.TRT',
+                    '2. name' => 'Bitcoin ETF',
+                    '3. type' => 'ETF',
+                    '4. region' => 'Toronto',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-05',
+                    '8. currency' => 'CAD',
+                    '9. matchScore' => '0.7778'
+                  },
+                  {
+                    '1. symbol' => 'EBIT.TRT',
+                    '2. name' => 'Bitcoin ETF CAD',
+                    '3. type' => 'ETF',
+                    '4. region' => 'Toronto',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-05',
+                    '8. currency' => 'CAD',
+                    '9. matchScore' => '0.6364'
+                  },
+                  {
+                    '1. symbol' => 'ADE.DEX',
+                    '2. name' => 'Bitcoin Group SE',
+                    '3. type' => 'Equity',
+                    '4. region' => 'XETRA',
+                    '5. marketOpen' => '08:00',
+                    '6. marketClose' => '20:00',
+                    '7. timezone' => 'UTC+02',
+                    '8. currency' => 'EUR',
+                    '9. matchScore' => '0.6087'
+                  },
+                  {
+                    '1. symbol' => 'ADE.FRK',
+                    '2. name' => 'Bitcoin Group SE',
+                    '3. type' => 'Equity',
+                    '4. region' => 'Frankfurt',
+                    '5. marketOpen' => '08:00',
+                    '6. marketClose' => '20:00',
+                    '7. timezone' => 'UTC+02',
+                    '8. currency' => 'EUR',
+                    '9. matchScore' => '0.6087'
+                  },
+                  {
+                    '1. symbol' => 'BTGGF',
+                    '2. name' => 'Bitcoin Group SE',
+                    '3. type' => 'Equity',
+                    '4. region' => 'United States',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-04',
+                    '8. currency' => 'USD',
+                    '9. matchScore' => '0.6087'
+                  },
+                  {
+                    '1. symbol' => 'BTM',
+                    '2. name' => 'Bitcoin Depot Inc',
+                    '3. type' => 'Equity',
+                    '4. region' => 'United States',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-04',
+                    '8. currency' => 'USD',
+                    '9. matchScore' => '0.5833'
+                  },
+                  {
+                    '1. symbol' => 'QBTC.TRT',
+                    '2. name' => 'Bitcoin Fund Unit',
+                    '3. type' => 'Equity',
+                    '4. region' => 'Toronto',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-05',
+                    '8. currency' => 'CAD',
+                    '9. matchScore' => '0.5833'
+                  },
+                  {
+                    '1. symbol' => 'BTGN',
+                    '2. name' => 'Bitcoin Generation Inc',
+                    '3. type' => 'Equity',
+                    '4. region' => 'United States',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-04',
+                    '8. currency' => 'USD',
+                    '9. matchScore' => '0.5455'
+                  },
+                  {
+                    '1. symbol' => 'BTMWW',
+                    '2. name' => 'Bitcoin Depot Inc Warrant',
+                    '3. type' => 'Equity',
+                    '4. region' => 'United States',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-04',
+                    '8. currency' => 'USD',
+                    '9. matchScore' => '0.4375'
+                  },
+                  {
+                    '1. symbol' => 'BTEUF',
+                    '2. name' => 'Bitcoin ETF ( USD Unhedged Units)',
+                    '3. type' => 'ETF',
+                    '4. region' => 'United States',
+                    '5. marketOpen' => '09:30',
+                    '6. marketClose' => '16:00',
+                    '7. timezone' => 'UTC-04',
+                    '8. currency' => 'USD',
+                    '9. matchScore' => '0.3500'
+                  }
+                ]
+              }
+            )
+          end
+        end
+      end
+
+      context 'when request returns no matches' do
+        it 'returns an empty array' do
+          VCR.use_cassette('alpha_vantage/symbol_search_success_empty') do
+            response = described_class.new.symbol_search(keywords: 'koetz')
+
+            expect(response).to eq({ 'bestMatches' => [] })
+          end
+        end
+      end
+    end
+
+    context 'when the request is not successful' do
+      context 'when Faraday::TimeoutError is raised' do
+        it 'raises an AlphaVantage::TimeoutError' do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::TimeoutError)
+
+          response = described_class.new.symbol_search(keywords: 'petr4')
+
+          expect(response).to be_nil
+          # expect(response['error']['name']).to eq('Faraday::TimeoutError')
+          # expect(response['error']['backtrace']).to be_present
+          # expect(response['error'].keys).to include('message')
+        end
+      end
+
+      context 'when Faraday::ConnectionFailed is raised' do
+        it 'raises an AlphaVantage::ConnectionFailedError' do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::ConnectionFailed)
+
+          response = described_class.new.symbol_search(keywords: 'petr4')
+
+          expect(response).to be_nil
+          # expect(response['error']['name']).to eq('Faraday::ConnectionFailed')
+          # expect(response['error']['backtrace']).to be_present
+          # expect(response['error'].keys).to include('message')
+        end
+      end
+
+      context 'when Faraday::ClientError is raised' do
+        it 'raises an AlphaVantage::ClientError' do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::ClientError)
+
+          response = described_class.new.symbol_search(keywords: 'petr4')
+
+          expect(response).to be_nil
+          # expect(response['error']['name']).to eq('Faraday::ClientError')
+          # expect(response['error']['backtrace']).to be_present
+          # expect(response['error'].keys).to include('message')
+        end
+      end
+
+      context 'when Faraday::ServerError is raised' do
+        it 'raises an AlphaVantage::ServerError' do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::ServerError)
+
+          response = described_class.new.symbol_search(keywords: 'petr4')
+
+          expect(response).to be_nil
+          # expect(response['error']['name']).to eq('Faraday::ServerError')
+          # expect(response['error']['backtrace']).to be_present
+          # expect(response['error'].keys).to include('message')
+        end
+      end
+
+      context 'when Faraday::ParsingError is raised' do
+        it 'raises an AlphaVantage::ParsingError' do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::ParsingError)
+
+          response = described_class.new.symbol_search(keywords: 'petr4')
+
+          expect(response).to be_nil
+          # expect(response['error']['name']).to eq('Faraday::ParsingError')
+          # expect(response['error'].keys).to include('message')
+        end
+      end
+    end
+  end
+end
