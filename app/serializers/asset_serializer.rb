4427b9a09129109f4664449ba8a25cba953ad157
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-04-12 12:20:36 -0300

    refactor: AssetSerializer to improve readability

--- app/serializers/asset_serializer.rb
+++ app/serializers/asset_serializer.rb
@@ -1,3 +1,4 @@
 class AssetSerializer < ActiveModel::Serializer
-  attributes :id, :code, :name, :business_name, :kind, :image_path, :custom, :user_id, :created_at, :updated_at
+  attributes :id, :code, :name, :business_name, :kind, :image_path,
+             :custom, :user_id, :created_at, :updated_at
 end
