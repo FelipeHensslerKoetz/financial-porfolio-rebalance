3b2af721e4b1c609523f5ec74c777661f915f806
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-04-14 22:32:07 -0300

    refactor: update allocation_weight and quantity ranges in investment_portfolio_assets factory

--- spec/factories/investment_portfolio_assets.rb
+++ spec/factories/investment_portfolio_assets.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
   factory :investment_portfolio_asset do
     asset { build(:asset) }
     investment_portfolio { build(:investment_portfolio) }
-    allocation_weight { rand(0.0..100.0) }
-    quantity { rand(0..100) }
+    allocation_weight { rand(0.1..100.0) }
+    quantity { rand(1..100) }
   end
 end
